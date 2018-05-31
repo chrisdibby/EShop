52bcb600deee8971441def1886334ef6da3ee2e8
Yurij Kolenko <chrisdibby@gmail.com>
2018-05-31 14:54:06 +0300

    App skeleton Order, Thing, ThingInOrder Entities

---
+++ logic/src/main/java/solutions/domain/Order.java
@@ -0,0 +1,31 @@
+package solutions.domain;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private long id;
+
+    @OneToMany
+    private List<ThingInOrder> thingInOrders;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<ThingInOrder> getThingInOrders() {
+        return thingInOrders;
+    }
+
+    public void setThingInOrders(List<ThingInOrder> thingInOrders) {
+        this.thingInOrders = thingInOrders;
+    }
+}
---
+++ logic/src/main/java/solutions/ejb/OrdersManagerBean.java
@@ -0,0 +1,4 @@
+package solutions.ejb;
+
+public class OrdersManagerBean {
+}
